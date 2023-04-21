0fef54e1bf2d9fa31d8838bc9715ce21a971fd73
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-04-21 17:33:50 +0700

    update

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+// const closeMenu = document.getElementById("close-menu");
+// const header_menu = document.getElementById("header_menu");
+// function openNav() {
+//   header_menu.classList.add = "header_menu_show";
+//   closeMenu.style.display = "block";
+// }
+// function closeNav() {
+//   header_menu.classList.remove = "header_menu_show";
+//   closeMenu.style.display = "none";
+// }
