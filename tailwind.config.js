384ba09f827f2388b95794caad1695bed132e90b
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-04-25 08:05:20 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    screens: {
-      xs: "480px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1440px",
-      "3xl": "1536px",
-      "4xl": "1920px",
-    },
     extend: {},
   },
   plugins: [],
-};
+}
