dd77f84f8b467cbcffe4cbe64361c9905804a3fa
dung0811 <103187128+dung2511@users.noreply.github.com>
2023-04-26 12:14:41 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,15 +31,7 @@ module.exports = {
             transform: "translateY(0)",
           },
         },
-        rotateAngle: {
-          "0%": {
-            transform: "rotate(0deg)",
-          },
-          "50%": { transform: "rotate(45deg)" },
-          "100%": {
-            transform: "rotate(90deg)",
-          },
-        },
+
         list_items_menu: {
           "0%": {
             transform: "translateY(10%)",
@@ -52,7 +44,6 @@ module.exports = {
       animation: {
         growDown: "growDown 0.4s ease-in-out",
         ansDown: "ansDown 0.4s ease-in-out",
-        rotateAngle: "rotateAngle 0.7s ease-in-out",
         list_items_menu: "list_items_menu 0.3s ease-in-out",
       },
     },
